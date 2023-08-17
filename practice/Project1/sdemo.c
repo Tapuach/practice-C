9bbe67c64d1114c52098365984c73f54b1ce5bb1
Tapuach <53472888+Tapuach@users.noreply.github.com>
2023-08-17 23:34:21 +0800

    Add files via upload

--- practice/Project1/sdemo.c
+++ practice/Project1/sdemo.c
@@ -3,9 +3,53 @@
 #include <stdio.h>
 #include <string.h>
 
+int main()
+{
+	int a = 8;
+	if (3 < a && a < 10)
+	{
+		printf("a in 3-10");
+	}
+	else
+	{
+		printf("sorry");
+	}
+
+
+	float f = 234.56;
+}
 
 
 
+
+//int main()
+//{
+//	int a = 4 + 5 * 2 - 6 / 3 + 11 % 4;
+//	printf("%d\n", a);
+//	scanf("%d", &a);
+//	printf("a=%d\n", a);
+//	while (a!=0)
+//	{
+//		printf("%d", a % 10);
+//		a = a / 10;
+//	}
+//}
+
+
+
+//int main()
+//{
+//	char a, b, c;
+//	a = 'ab';
+//	b = 'b';
+//	c = 'c';
+//	putchar(a);
+//	putchar('\b');
+//	putchar(b);
+//	putchar(c);
+//	putchar('\n');
+//	system("pause");
+//}
 
 
 
